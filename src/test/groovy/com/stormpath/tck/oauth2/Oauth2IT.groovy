d5bbc6ce8d25c4f38b4f46d0fc6b0b2734b8dd31
Nate Barbettini <nathanaelb@gmail.com>
2016-03-09 13:59:36 -0800

    Test password grant flow with username (closes #11)

--- src/test/groovy/com/stormpath/tck/oauth2/Oauth2IT.groovy
+++ src/test/groovy/com/stormpath/tck/oauth2/Oauth2IT.groovy
@@ -15,8 +15,15 @@
  */
 package com.stormpath.tck.oauth2
 
+import com.jayway.restassured.http.ContentType
+import com.jayway.restassured.response.Response
 import com.stormpath.tck.AbstractIT
+import com.stormpath.tck.login.LoginJsonIT
+import com.stormpath.tck.util.RestUtils
+import org.testng.annotations.BeforeClass
 import org.testng.annotations.Test
+import sun.reflect.generics.reflectiveObjects.NotImplementedException
+
 import static com.jayway.restassured.RestAssured.*
 import static com.jayway.restassured.matcher.RestAssuredMatchers.*
 import static org.hamcrest.Matchers.*
@@ -24,18 +31,48 @@ import static org.hamcrest.Matchers.*
 @Test
 class Oauth2IT extends AbstractIT {
 
+    private final String randomUUID = UUID.randomUUID().toString()
+    private final String accountEmail = "fooemail-" + randomUUID + "@stormpath.com"
+    private final String accountUsername = randomUUID
+    private final String accountPassword = "P@ssword123"
+
+    @BeforeClass
+    private void createTestAccount() throws Exception {
+
+        Map<String, Object>  jsonAsMap = new HashMap<>();
+        jsonAsMap.put("email", accountEmail)
+        jsonAsMap.put("password", accountPassword)
+        jsonAsMap.put("givenName", "GivenName-" + randomUUID)
+        jsonAsMap.put("surname", "Surname-" + randomUUID)
+        jsonAsMap.put("username", accountUsername)
+
+        String createdHref =
+                given()
+                    .contentType(ContentType.JSON)
+                    .body(jsonAsMap)
+                .when()
+                    .post("/register")
+                .then()
+                    .statusCode(200)
+                .extract()
+                    .path("account.href")
+
+        deleteOnClassTeardown(createdHref)
+    }
+
     @Test
     /**
      * @see <a href="https://github.com/stormpath/stormpath-framework-tck/issues/6">#6</a>
      */
     public void unsupportedGrantType() throws Exception {
+
         given()
             .param("grant_type", "foobar_grant")
         .when()
             .post("/oauth/token")
         .then()
             .statusCode(400)
-            .header("Content-Type", is("application/json;charset=UTF-8"))
+            .contentType(ContentType.JSON)
             .header("Cache-Control", is("no-store"))
             .header("Pragma", is("no-cache"))
             .body("error", is("unsupported_grant_type"))
@@ -46,13 +83,14 @@ class Oauth2IT extends AbstractIT {
      * @see <a href="https://github.com/stormpath/stormpath-framework-tck/issues/7">#7</a>
      */
     public void missingGrantType() throws Exception {
+
         given()
             .param("grant_type", "")
         .when()
             .post("/oauth/token")
         .then()
             .statusCode(400)
-            .header("Content-Type", is("application/json;charset=UTF-8"))
+            .contentType(ContentType.JSON)
             .header("Cache-Control", is("no-store"))
             .header("Pragma", is("no-cache"))
             .body("error", is("invalid_request"))
@@ -63,13 +101,14 @@ class Oauth2IT extends AbstractIT {
      * @see <a href="https://github.com/stormpath/stormpath-framework-tck/issues/15">#15</a>
      */
     public void missingFormParameters() throws Exception {
+
         given()
             .body("""hello"" : ""world""")
         .when()
             .post("/oauth/token")
         .then()
             .statusCode(400)
-            .header("Content-Type", is("application/json;charset=UTF-8"))
+            .contentType(ContentType.JSON)
             .header("Cache-Control", is("no-store"))
             .header("Pragma", is("no-cache"))
             .body("error", is("invalid_request"))
@@ -84,5 +123,25 @@ class Oauth2IT extends AbstractIT {
         get("/oauth/token")
             .then()
             .assertThat().statusCode(405)
+    }
+
+    /**
+     * @see <a href="https://github.com/stormpath/stormpath-framework-tck/issues/11">#11</a>
+     */
+    public void passwordGrantWithUsername() throws Exception {
+
+        given()
+            .param("grant_type", "password")
+            .param("username", accountEmail)
+            .param("password", accountPassword)
+        .when()
+            .post("/oauth/token")
+        .then()
+            .statusCode(200)
+            .contentType(ContentType.JSON)
+            .body("access_token", not(isEmptyOrNullString()))
+            .body("expires_in", is(3600))
+            .body("refresh_token", not(isEmptyOrNullString()))
+            .body("token_type", is("Bearer"))
     }
 }
